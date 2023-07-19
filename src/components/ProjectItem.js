2edbe3592a42fa2dcb2932a0fc6d20c9514e18f8
Gitau Njung'e <gitau.njunge@student.moringaschool.com>
2023-07-19 20:21:39 +0300

    done for now

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,15 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  const technologieS = technologies.map((tech, name) => {
+    return <span key={name}>{tech}</span>
+  })
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {technologieS}
       </div>
     </div>
   );
